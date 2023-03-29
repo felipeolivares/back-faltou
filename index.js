42f7972c063b4ab1d842f26cfc5358982b7f8329
Felipe Olivares Campos <felipeolivares99@hotmail.com>
2023-03-29 19:18:48 -0300

    new port

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import express from "express";
 import userRoutes from "./routes/users.js";
 import subjectRoutes from "./routes/subjects.js";
 import cors from "cors";
+import { config } from 'dotenv'
+config()
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
 
 app.use("/", userRoutes)
 app.use("/calculation", subjectRoutes)
